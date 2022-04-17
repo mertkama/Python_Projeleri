836060fc362d5909e0d97e2a300b3fd9580e4163
mert <mert172317@gmail.com>
2022-04-17 16:53:06 +0300

    First commit

---
+++ kullanici_adi_sifre/kadi_ksifre.py
@@ -0,0 +1,5 @@
+from kullanici_adi_sifre import kadi_sifre_kontrol as kullanici
+isim = kullanici.kullanici_adi()
+sifre = kullanici.kullanici_sifre()
+print("kullanıcı adınız {}, kullanıcı şifreniz {}".format(isim,sifre))
+
